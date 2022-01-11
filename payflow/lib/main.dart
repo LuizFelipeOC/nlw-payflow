1cd87e5f2463781f1ea578ec6292eca51e85062d
Luiz Felipe <luizfelipeeoliveiraac@gmail.com>
2022-01-11 09:34:52 -0400

    refactor: finalizada a aula 2 da nlw

--- payflow/lib/main.dart
+++ payflow/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/splash/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +11,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
     );
   }
 }
